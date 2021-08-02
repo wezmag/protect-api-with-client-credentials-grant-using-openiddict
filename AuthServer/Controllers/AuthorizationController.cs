a19cbfdb823565c4b4bf28c9f2237dfbe0f8d3ad
wesley.huang <wesley.huang@tw.promise.com>
2021-08-02 15:49:30 +0800

    add token endpoint

---
+++ AuthServer/Controllers/AuthorizationController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using OpenIddict.Abstractions;
+using OpenIddict.Server.AspNetCore;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using static OpenIddict.Abstractions.OpenIddictConstants;
+
+namespace AuthServer.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        private readonly IOpenIddictApplicationManager _applicationManager;
+
+        public AuthorizationController(
+            IOpenIddictApplicationManager applicationManager
+            )
+        {
+            _applicationManager = applicationManager;
+        }
+
+        [HttpPost("~/connect/token"), Produces("application/json")]
+        public async Task<IActionResult> Exchange()
+        {
+            var request = HttpContext.GetOpenIddictServerRequest() ??
+                throw new InvalidOperationException("The OpenID Connect request cannot be retrieved.");
+
+
+            if (request.IsClientCredentialsGrantType())
+            {
+                var application = await _applicationManager.FindByClientIdAsync(request.ClientId);
+                if (application == null)
+                {
+                    throw new InvalidOperationException("The application details cannot be found in the database.");
+                }
+
+                var identity = new ClaimsIdentity(TokenValidationParameters.DefaultAuthenticationType, Claims.Name, Claims.Role);
+
+                // 將 claim (sub=clientid) 加到 access token, id_token
+                identity.AddClaim(Claims.Subject, await _applicationManager.GetClientIdAsync(application),
+                    Destinations.AccessToken, Destinations.IdentityToken);
+
+                // 將 claim (name=client display name) 加到 access token, id_token
+                identity.AddClaim(Claims.Name, await _applicationManager.GetDisplayNameAsync(application),
+                    Destinations.AccessToken, Destinations.IdentityToken);
+
+                var principal = new ClaimsPrincipal(identity);
+                
+                return SignIn(principal, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+            }
+
+            throw new InvalidOperationException("The specified grant type is not supported.");
+        }
+    }
+}
